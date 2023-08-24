0500db14333588149a2246f37550902cf8276d8a
garbhank <garbhan@me.com>
2023-08-24 20:00:53 +0100

    turn main into separate funcs

--- main.go
+++ main.go
@@ -10,22 +10,16 @@ import (
 	"golang.design/x/clipboard"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func readSQL(fileName string) string {
+	f, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
 	}
+	fileString := string(f)
+	return fileString
 }
 
-func main() {
-	fmt.Println("Hello bqt!")
-
-	// read in sql file
-	f, err := os.ReadFile("wow.sql")
-	check(err)
-	// fmt.Print(string(f))
-	sqlFile := string(f)
-
-	// read from json config to map template value to string
+func createMapping() map[string]string {
 	mappingFile, err := os.ReadFile("mappings.json")
 	if err != nil {
 		panic(err)
@@ -35,33 +29,48 @@ func main() {
 	json.Unmarshal([]byte(mappingFile), &m)
 	fmt.Println(m)
 
-	// make ds_noj=dash current_date
+	// make ds_nodash current_date
 	dt := time.Now()
 	ds_nodash := fmt.Sprintf("%d-%d-%d", dt.Year(), dt.Month(), dt.Day())
 	m["ds_nodash"] = ds_nodash
 
+	return m
+}
+
+func exportToClipboard(templatedStr string) {
+	err := clipboard.Init()
+	if err != nil {
+		fmt.Printf("Unable to init clipboard, %s", err.Error())
+	}
+
+	byteSql := []byte(templatedStr)
+	clipboard.Write(clipboard.FmtText, byteSql)
+}
+
+func main() {
+	fmt.Println("Hello bqt!")
+
+	// read in sql file
+	sqlFile := readSQL("wow.sql")
+
+	// template/value mapping from 'mapping.json'
+	m := createMapping()
+
 	var formatString string
 	tempFile := strings.Clone(sqlFile)
+
 	for k, v := range m {
+		// TODO: add branch for terraform/airflow
 		template := fmt.Sprintf("{{ %s }}", k)
 		formatString = strings.ReplaceAll(tempFile, template, v)
 		tempFile = strings.Clone(formatString)
 	}
 	fmt.Println(formatString)
 
-	// text replace the template vars "{{ x }}"
-	// fmtdString := strings.ReplaceAll(string(sqlFile), "{{ params.project }}", "gk-africa-data-eu-dev")
-	// fmt.Print(fmtdString)
-
-	// return output to clipboard
-	err = clipboard.Init()
-	if err != nil {
-		fmt.Printf("Unable to init clipboard, %s", err.Error())
-	}
-
-	clipboard.Write(clipboard.FmtText, []byte("text data"))
+	exportToClipboard(formatString)
 
 	curr_clipboard := clipboard.Read(clipboard.FmtText)
 	fmt.Println(string(curr_clipboard))
 
+	fmt.Println("byebye")
 }
