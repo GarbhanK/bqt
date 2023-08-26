cf1d94097e4429609cecf370c9c3eea2744106e2
garbhank <garbhan@me.com>
2023-08-27 00:18:03 +0100

    terraform templater test

--- src/main/main_test.go
+++ src/main/main_test.go
@@ -17,8 +17,28 @@ func TestReadSQL(t *testing.T) {
 	sql += "group by insertionTimestamp desc"
 	expected := sql
 
-	t.Logf("result: %s\n", result)
-	t.Logf("expected: %s\n", expected)
+	if len(result) > 0 {
+		t.Logf("ReadSQL('test.sql') PASSED. Is not an empty string\n")
+	} else {
+		t.Errorf("ReadSQL('test.sql') FAILED. Got an empty string\n")
+	}
+
+	if result != expected {
+		t.Errorf("ReadSQL('test.sql') FAILED. Expected %s, got %s\n", expected, result)
+	} else {
+		t.Logf("ReadSQL('test.sql') PASSED. Expected %s, got %s\n", expected, result)
+	}
+}
+
+func TestReadSQLTerraform(t *testing.T) {
+   	result := ReadSQL("terraform_template.sql")
+
+	sql := "select * from `${params.project}.transactions.coffee` c\n"
+	sql += "where date(insertionTimestamp) >= '${ds_nodash}'\n"
+	sql += "left join `${params.web_project}.unified_segment.tracks` t\n"
+	sql += "on c.userId = t.userId\n"
+	sql += "group by insertionTimestamp desc"
+	expected := sql
 
 	if len(result) > 0 {
 		t.Logf("ReadSQL('test.sql') PASSED. Is not an empty string\n")
