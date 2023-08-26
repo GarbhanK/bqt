503788105d0f6fbbe5f9719ad2378eb5bfa02abe
garbhank <garbhan@me.com>
2023-08-26 23:54:50 +0100

    build scripts and linux binary

---
+++ scripts/build-linux.go
@@ -0,0 +1,2 @@
+go build -o ~/Code/bqt/bin/bqt-linux ~/Code/bqt/src/main/main.go
+sudo cp ~/Code/bqt/bin/bqt-linux /usr/bin
---
+++ scripts/macos-build.go
@@ -0,0 +1,2 @@
+go build -o ~/Developer/misc/bqt/bin/bqt ~/Developer/misc/bqt/src/main/main.go
+sudo cp ~/Developer/misc/bqt/bin/bqt /usr/bin
