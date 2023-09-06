da40df120d9ef36a6ad1629d66c226323266ee59
Garbhan Kenneally <garbhan@me.com>
2023-09-06 15:14:03 +0100

    fixed shell script filetype and new macos binary build

---
+++ scripts/build-macos.sh
@@ -0,0 +1,3 @@
+go build -o ~/Developer/bqt/bin/bqt ~/Developer/bqt/src/main/main.go
+echo "Build complete"
+# sudo cp ~/Developer/bqt/bin/bqt /usr/bin
--- scripts/macos-build.go
+++
@@ -1,2 +0,0 @@
-go build -o ~/Developer/misc/bqt/bin/bqt ~/Developer/misc/bqt/src/main/main.go
-sudo cp ~/Developer/misc/bqt/bin/bqt /usr/bin
